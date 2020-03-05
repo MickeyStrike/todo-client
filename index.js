730c8ba9f707f0201358d3b3606d52e1b745feff
MickeyStrike <irfanmaulana281299@gmail.com>
2020-03-06 02:01:03 +0700

    add feature

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 let token = localStorage.getItem('token')
+
+// Google Sign In
+function onSignIn(googleUser) {
+    let profile = googleUser.getBasicProfile();
+    let id_token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        method:"POST",
+        url:"http://localhost:3000/loginGoogle",
+        data:{
+            email:profile.getEmail(),
+            password:"defaultpassword",
+            id_token
+        }
+    })
+    .done((res) => {
+        $('#form-login').hide()
+        $('#home').hide()
+        $('#dashboard').show()
+        localStorage.setItem('token',res.token)
+    })
+    .fail((err) => {
+        console.log(err)
+    })
+}
+// Document Ready
 $(document).ready(() => {
     if(token) {
-        $('#dashboard').show()
+        $('#dashboard').hide()
         $('#home').hide()
         $('#form-register').hide()
         $('#form-login').hide()
+        $('#page-explore').show()
     }else{
         $('#dashboard').hide()
         $('#home').show()
@@ -12,15 +38,17 @@ $(document).ready(() => {
         $('#form-login').hide()
     }
 
-    $("#login").on("click",(e) => {
+    $(".login").on("click",(e) => {
         e.preventDefault()
         $("#home").hide()
+        $('#form-register').hide()
         $("#form-login").show()
     })
 
-    $("#register").on("click",(e) => {
+    $(".register").on("click",(e) => {
         e.preventDefault()
         $("#home").hide()
+        $("#form-login").hide()
         $("#form-register").show()
     })
 
@@ -71,11 +99,41 @@ $(document).ready(() => {
     $('#btn-logout').on("click",(e) => {
         e.preventDefault()
         localStorage.removeItem('token')
+        // Google Sign Out
+        let auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            console.log('User Sign Out')
+        });
         $('#dashboard').hide()
         $('#home').show()
     })
 
-    $('#btn-btn-google-sign-in').on("click",(e) => {
-        
-    })
+    $("#explore").click(function(e){
+        $.ajax({
+            method:"GET",
+            url:"http://localhost:3000/todos"
+        })
+        .done((res) => {
+            console.log(res)
+            res.result.forEach(el => {
+                // $(".modal-body").append(`<p>${el.tittle}</p><br><p>${el.description}</p>`)
+                $(".modal-body").append(`<div class="col-md-6 mb-4">
+                <!-- Card -->
+                  <div class="card hoverable">
+                      <!-- Card content -->
+                      <div class="card-body">
+                          <div class="media">
+                              <span class="card-img-100 d-inline-flex justify-content-center align-items-center rounded-circle grey lighten-3 mr-4">
+                                  <i class="far fa-user fa-2x purple-text"></i>
+                              </span>
+                          <div class="media-body">
+                              <h5 class="dark-grey-text mb-3">${el.tittle}</h5>
+                              <p class="font-weight-light text-muted mb-0">${el.description}</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>`)
+            });
+        })
+    }) 
 })
